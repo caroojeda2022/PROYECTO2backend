00c7eb07a0c3ca0e65a523f3d8523806d44ddada
caroojeda2022 <102704165+caroojeda2022@users.noreply.github.com>
2022-06-09 14:22:12 +0200

    Update modifyNote.js

--- controllers/Note/modifyNote.js
+++ controllers/Note/modifyNote.js
@@ -1,1 +1,18 @@
+const selectmodifyNoteQuery = require('../../db/modifyNoteQueries/selectNoteQuery');
 
+const modifyNote = async (req, res) => {
+    try {
+        const modifyNote = await selectNoteQuery();
+
+        res.send({
+            status: 'ok',
+            data: {
+                note,
+            },
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = modifyNote;
