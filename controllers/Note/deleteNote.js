47c88a5875cefa6df7954a15481abcba37e6a948
caroojeda2022 <102704165+caroojeda2022@users.noreply.github.com>
2022-06-09 14:18:58 +0200

    Update deleteNote.js

--- controllers/Note/deleteNote.js
+++ controllers/Note/deleteNote.js
@@ -1,1 +1,18 @@
+const deleteNoteQuery = require('../../db/tablesQueries/deleteNoteQuery');
 
+const deleteNote = async (req, res) => {
+    try {
+        const { Note } = req.params;
+
+        await deleteNoteQuery(Note);
+
+        res.send({
+            status: 'ok',
+            message: 'Nota eliminada',
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = deleteNote;
