81fdff041e1dc6adf09fa8f47c667859ba76c7f0
caroojeda2022 <102704165+caroojeda2022@users.noreply.github.com>
2022-06-09 14:21:36 +0200

    Update index.js

--- controllers/Note/index.js
+++ controllers/Note/index.js
@@ -1,1 +1,13 @@
+const newNote = require('./newNote');
+const titleNote = require('./titleNote');
+const modifyNote = require('./modifyNote');
+const categoryNote = require('./categoyNote');
+const deleteNote = require('./deleteNote');
 
+module.exports = {
+    newNote,
+    titleNote,
+    modifyNote,
+    categoryNote,
+    deleteNote,
+};
