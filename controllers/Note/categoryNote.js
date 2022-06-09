a783cc03a53217d67bbd7f59e305cd06b3df594c
caroojeda2022 <102704165+caroojeda2022@users.noreply.github.com>
2022-06-09 14:17:49 +0200

    Update categoryNote.js

--- controllers/Note/categoryNote.js
+++ controllers/Note/categoryNote.js
@@ -1,1 +1,26 @@
+const insertcategoryNoteQuery = require('../../db/noteQueries/categoryNoteQuery');
 
+/* FALTA COLOCAR LAS CATEGORIAS PUBLICO O PRIVADO A MODIFICAR */
+
+const categoryNote = async (req, res, next) => {
+    try {
+        const { name } = req.body;
+
+        if (!name) {
+            const err = new Error('Faltan campos');
+            err.statusCode = 400;
+            throw err;
+        }
+
+        await categoryNoteQuery(note);
+
+        res.send({
+            status: 'ok',
+            message: 'Nota agregada',
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = categoryNote;
