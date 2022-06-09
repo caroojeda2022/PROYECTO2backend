ecf129b10e897c2d1116cd60ffc3754624a0c242
caroojeda2022 <102704165+caroojeda2022@users.noreply.github.com>
2022-06-09 14:23:37 +0200

    Update titleNote.js

--- controllers/Note/titleNote.js
+++ controllers/Note/titleNote.js
@@ -1,1 +1,18 @@
+const selectitleNoteQuery = require('../../db/titleNoteQueries/selecttitleNoteQuery');
 
+const listNote = async (req, res, next) => {
+    try {
+        const note = await selecttitleNoteQuery();
+
+        res.send({
+            status: 'ok',
+            data: {
+                note,
+            },
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = titleNote;
