2f0f78c474561e7c4141ecec4ca6468528098e92
caroojeda2022 <102704165+caroojeda2022@users.noreply.github.com>
2022-06-09 14:22:57 +0200

    Update newNote.js

--- controllers/Note/newNote.js
+++ controllers/Note/newNote.js
@@ -1,1 +1,25 @@
-newNote.js
+const insertNoteQuery = require('../../db/noteQueries/insertNoteQuery');
+
+// crear una nueva nota 
+const newNote = async (req, res) => {
+    try {
+        const { name } = req.body;
+
+        if (!name) {
+            const err = new Error('La nota no puede estar vacía');
+            err.statusCode = 400;
+            throw err;
+        }
+
+        await insertNoteQuery(note);
+
+        res.send({
+            status: 'ok',
+            message: 'Nota agregada',
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+module.exports = newNote;
